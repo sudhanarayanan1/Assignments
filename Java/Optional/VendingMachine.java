5c390b1057470171433ff31894d4db693554f777
sudhanarayanan1 <89568407+sudhanarayanan1@users.noreply.github.com>
2021-11-17 21:47:16 +0530

    Commit 4

--- Java/Optional/VendingMachine.java
+++ Java/Optional/VendingMachine.java
@@ -1,8 +1,11 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class VendingMachine {
     static String[][] itemArray;
+    static ArrayList<String> transactionList;
     static int vendingMachineBalance = 0;
 
     // Print Main menu options
@@ -11,10 +14,12 @@ public class VendingMachine {
         System.out.println("Enter the option: ");
         System.out.println("1. Buy item" );
         System.out.println("2. View Vending Machine balance");
+        System.out.println("3. View all previous transactions");
     }
 
     // Initialize product details of VM
     public  static void setupVendingMachine() {
+        transactionList = new ArrayList<>();
         itemArray = new String[][]{
                 {"001", "Chips", "40"},
                 {"002", "Biscuit", "30"},
@@ -23,7 +28,7 @@ public class VendingMachine {
         };
     }
 
-    // Print formatted Vending machine menu
+    // Print formatted Vending machine item menu
     public static void printVendingMachineMenu() {
         System.out.printf("%-22s%-22s%-22s\n","Item ID","Item Name","Price");
         System.out.printf("%-22s%-22s%-22s\n",itemArray[0][0], itemArray[0][1], itemArray[0][2]);
@@ -34,15 +39,28 @@ public class VendingMachine {
 
     // Dispense item, Return change, Update Vending machine balance
     public static void dispenseAndReturnChange(int id, int amount) {
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String transaction = null;
         int itemPrice = Integer.parseInt(itemArray[id-1][2]);
         if (itemPrice <= amount)
         {
             System.out.println("Dispensing " + itemArray[id-1][1]);
             System.out.println("Returning change of Rs " + (amount - itemPrice));
             vendingMachineBalance = vendingMachineBalance + Integer.parseInt(itemArray[id-1][2]);
+            transaction = timeStamp + Integer.toString(id) + "   \t" + Integer.toString(id) + "\t\t\t" + Integer.toString(amount) + "\t\t" + Integer.toString(amount-itemPrice) + "\t\t" + Integer.toString(vendingMachineBalance);
+            transactionList.add(transaction);
             return;
         }
         System.out.println("Insufficient amount. Try again.");
+    }
+
+    // Printing all vending machine transactions
+    private static void printTransactions() {
+        System.out.println("Date&Time" + "\t\t\t\t" + "Item" + "\t" + "Amount" + "\t" + "Refund" + "\t" + "Vending Machine balance");
+        for (int i=0; i<transactionList.size(); i++) {
+            String transaction = transactionList.get(i);
+            System.out.println(transaction);
+        }
     }
 
     public static void main(String[] args) {
@@ -54,24 +72,34 @@ public class VendingMachine {
         do {
             printOptions();
             option = s.nextInt();
-            
-            if (option == 1) {
-                printVendingMachineMenu();
-                // Take user input for item id and amount
-                try {
-                    System.out.println("Enter Item ID: ");
-                    int id = s.nextInt();
-                    System.out.println("Enter amount: ");
-                    int amount = s.nextInt();
-                    dispenseAndReturnChange(id, amount);
-                } catch (InputMismatchException e) {
-                    System.out.println("Entered input is not a number");
+
+            switch (option) {
+                case 1:
+                    printVendingMachineMenu();
+                    // Take user input for item id and amount
+                    try {
+                        System.out.println("Enter Item ID: ");
+                        int id = s.nextInt();
+                        if (id < 1 || id > 4) {
+                            throw new InputMismatchException();
+                        }
+                        System.out.println("Enter amount: ");
+                        int amount = s.nextInt();
+                        dispenseAndReturnChange(id, amount);
+                        } catch (InputMismatchException e) {
+                            System.out.println("Entered input is invalid");
+                        }
+                        break;
+                case 2:
+                        System.out.println("Vending machine balance is: Rs " + vendingMachineBalance);
+                        break;
+                case 3:
+                        printTransactions();
+                        break;
+                default:
+                        System.out.println("Invalid option");
+                        break;
                 }
-            } else if (option == 2) {
-                System.out.println("Vending machine balance is: Rs " + vendingMachineBalance);
-            } else {
-                System.out.println("Invalid option");
-            }
 
             System.out.println("Do you want to continue (y/n)?: ");
             reply = s.next().charAt(0);
